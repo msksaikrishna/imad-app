7e41503066241a3acfa26c65ca8fdf3a21caac8f
msksaikrishna <37344431+msksaikrishna@users.noreply.github.com>
2018-04-18 17:11:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function(req, res){
-    res.send('Article one requested and will be served here');
-});
+app.get('/article-one', function(req, res){{
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+}});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
